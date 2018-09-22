94c74711e01f675556c2860702e8d9d821a10381
standmit <standmit@yandex.ru>
2018-09-22 22:30:06 +0700

    add family L0

--- stm32x_hal.h
+++ stm32x_hal.h
@@ -102,6 +102,24 @@
 		defined (STM32H750xx)
 #include "stm32h7xx_hal.h"
 
+#elif	defined (STM32L011xx) || \
+		defined (STM32L021xx) || \
+		defined (STM32L031xx) || \
+		defined (STM32L041xx) || \
+		defined (STM32L051xx) || \
+		defined (STM32L052xx) || \
+		defined (STM32L053xx) || \
+		defined (STM32L061xx) || \
+		defined (STM32L062xx) || \
+		defined (STM32L063xx) || \
+		defined (STM32L071xx) || \
+		defined (STM32L072xx) || \
+		defined (STM32L073xx) || \
+		defined (STM32L081xx) || \
+		defined (STM32L082xx) || \
+		defined (STM32L083xx)
+#include "stm32l0xx_hal.h"
+
 #else
 #warning Target MCU is undefined. The __FILE__ is useless.
 
